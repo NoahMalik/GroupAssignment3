7505e7749acea9fff3be3de24e9a9dd6fe6ddc11
NoahMalik <nhmkb9@umsystem.edu>
2025-04-05 22:49:29 -0500

    Adding some minor tweaks

--- src/pattern/LegatoMidiEventFactory.java
+++ src/pattern/LegatoMidiEventFactory.java
@@ -2,21 +2,19 @@ package pattern;
 
 import javax.sound.midi.*;
 
+import model.MidiEventData;
+
 public class LegatoMidiEventFactory implements MidiEventFactory {
 
     @Override
-    public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
-        // Create a Legato-style Note On event (you can modify it to implement legato behavior)
-        ShortMessage message = new ShortMessage();
-        message.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
-        return new MidiEvent(message, tick);
+    public MidiEventData createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+        // Create MidiEventData for "Note On"
+        return new MidiEventData(tick, velocity, note, channel, ShortMessage.NOTE_ON);
     }
 
     @Override
-    public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
-        // Create a Note Off event (you can modify it to implement legato behavior)
-        ShortMessage message = new ShortMessage();
-        message.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
-        return new MidiEvent(message, tick);
+    public MidiEventData createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+        // Create MidiEventData for "Note Off"
+        return new MidiEventData(tick, 0, note, channel, ShortMessage.NOTE_OFF); // 0 velocity for note off
     }
 }
